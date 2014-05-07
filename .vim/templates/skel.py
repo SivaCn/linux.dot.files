fbc9dc3830ce2fb690a498271f9547360db208d1
Siva Cn <cnsiva.in@gmail.com>
2014-05-08 02:51:21 +0530

    Create skel.py

---
+++ .vim/templates/skel.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+
+## ------------ Imports ----------- ##
+## ------------ Imports ----------- ##
+
+
+if __name__ == '__main__':
+    """This Bolck is used for Unit Test.
+    """
+    pass
