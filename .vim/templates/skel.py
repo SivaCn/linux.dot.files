af37be9084237fcb16ed15f4a498f2db4001ab7c
Siva Cn <cnsiva.in@gmail.com>
2017-05-14 00:58:07 +0530

    Update skel.py

--- .vim/templates/skel.py
+++ .vim/templates/skel.py
@@ -1,8 +1,27 @@
-#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
+"""
 
-## ------------ Imports ----------- ##
-## ------------ Imports ----------- ##
+    Module :mod:``
+
+    This Module is created to...
+
+    LICENSE: The End User license agreement is located at the entry level.
+
+"""
+
+# ----------- START: Native Imports ---------- #
+# ----------- END: Native Imports ---------- #
+
+# ----------- START: Third Party Imports ---------- #
+# ----------- END: Third Party Imports ---------- #
+
+# ----------- START: In-App Imports ---------- #
+# ----------- END: In-App Imports ---------- #
+
+__all__ = [
+    # All public symbols go here.
+]
 
 
 if __name__ == '__main__':
