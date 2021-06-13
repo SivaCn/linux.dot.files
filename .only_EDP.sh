5600b68f1413a8ee9856edcff5df88e76fec216c
Siva Cn <sivacn@biglittle.ai>
2021-06-13 21:48:43 +0530

    updates

---
+++ .only_EDP.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+xrandr --output eDP-1 --auto --primary
+xrandr --output HDMI-1 --off
+
+feh --bg-scale -F ~/Wallpaper/Arch-1.jpg
--- .only_HDMI.sh
+++ .only_HDMI.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-xrandr --output eDP-1 --off
 xrandr --output HDMI-1 --auto --primary
+xrandr --output eDP-1 --off
 
 feh --bg-scale -F ~/Wallpaper/Arch-1.jpg
